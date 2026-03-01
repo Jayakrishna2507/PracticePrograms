52e936a1dad267620fe86d1ebb24b04222db14f0
Jayakrishna2507 <jm0966@srmist.edu.in>
2026-03-01 20:44:40 +0530

    ADDED PROGRAMS

---
+++ L1P1.java
@@ -0,0 +1,15 @@
+/**
+ * @author [JAYAKRISHNA M]
+ * @version 1.0 UC5-Inline Array Initialization
+ */
+public class age {
+
+    public static void main(String[] args) {
+
+        int year = 2000;
+        int thisYear = 2024;
+        int ageValue = thisYear - year;
+
+        System.out.println("Harry's age in 2024 is " + ageValue);
+    }
+}
---
+++ L1P3.java
@@ -0,0 +1,11 @@
+class pp2
+{
+    static void main()
+    {
+        int m=94;
+        int p=95;
+        int c=96;
+        int avg=(m+p+c)/3;
+        System.out.println("Sam's average mark in PCM is "+avg);
+    }
+}
